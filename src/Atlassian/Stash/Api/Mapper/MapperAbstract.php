dc1d717f376d6ea2c09b5a6f20c70ee044017ffb
Grzegorz Godlewski <grzegorz@sorcerysutdio.com>
2016-12-20 23:49:36 +0100

    Mapper responsibility clarified

--- src/Atlassian/Stash/Api/Mapper/MapperAbstract.php
+++ src/Atlassian/Stash/Api/Mapper/MapperAbstract.php
@@ -2,6 +2,10 @@
 
 namespace Atlassian\Stash\Api\Mapper;
 
+/**
+ * The role of an mapper is to translate the raw API response
+ * to the entity.
+ */
 abstract class MapperAbstract
 {
     public function allFromPhpData(array $data)
